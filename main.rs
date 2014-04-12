17428ac479d2b6b42df884cc309305792b243c36
Simon Brenner <olsner@gmail.com>
2014-04-12 22:54:18 +0200

    Actual number formatting

--- main.rs
+++ main.rs
@@ -6,6 +6,7 @@
 
 extern crate core;
 
+use core::cmp::min;
 use core::container::Container;
 use start32::MultiBootInfo;
 
@@ -30,11 +31,55 @@ fn range(lo: uint, hi: uint, it: |uint| -> ()) {
 }
 
 trait Writer {
-	fn write(&mut self, string : &str);
-}
+	fn putc(&mut self, c : char);
 
-fn writeUInt<T : Writer>(out : &mut T, x : uint) {
-	out.write("<num>");
+	fn write(&mut self, string : &str) {
+		range(0, string.len(), |i| {
+			self.putc(string[i] as char);
+		});
+	}
+
+	fn writeUInt(&mut self, x : uint) {
+		self.writeNumber(0, false, 10, false, x);
+	}
+
+	fn writeNumber(&mut self, width : uint, leading_zero : bool, base : uint, show_base : bool, num : uint)
+	{
+		if show_base && base == 16 {
+			self.write("0x");
+		}
+		let mut buf : [u8, ..32] = [0, ..32];
+		let mut len = 0;
+		let mut num_ = num;
+		loop {
+			let i = num_ % base;
+			buf[len] = "0123456789abcdef"[i];
+			len += 1;
+			num_ /= base;
+			if num_ == 0 { break; }
+		}
+		if width > 0 {
+			let c = if leading_zero { '0' } else { ' ' };
+			range(0, min(len, width), |_| {
+				self.putc(c);
+			});
+		}
+		while len > 0 {
+			len -= 1;
+			self.putc(buf[len] as char);
+		}
+	}
+
+	fn writeSNumber(&mut self, width : uint, leading_zero : bool, num : int) {
+		let abs = if num < 0 {
+			self.putc('-');
+			-num
+		} else {
+			num
+		} as uint;
+		self.writeNumber(width, leading_zero, 10, false, abs);
+	}
+
 }
 
 struct Console {
@@ -57,6 +102,9 @@ impl Console {
 	}
 
 	fn putchar(&mut self, position : uint, c : u16) {
+		unsafe {
+			*mut_offset(self.buffer, position as int) = c;
+		}
 	}
 
 	fn clear(&mut self) {
@@ -65,21 +113,53 @@ impl Console {
 		}});
 		self.position = 0;
 	}
+
+	fn clear_eol(&mut self) {
+		range(0, self.width - (self.position % self.width), |i| {
+			self.putchar(self.position + i, 0);
+		});
+	}
+
+	fn copy_back(&mut self, to : uint, from : uint, n : uint) {
+		range(0, n, |i| {
+			unsafe {
+				*mut_offset(self.buffer, (to + i) as int) =
+					*mut_offset(self.buffer, (from + i) as int);
+			}
+		});
+	}
+
+	fn scroll(&mut self, lines : uint) {
+		if lines >= self.height {
+			self.clear();
+			return;
+		}
+		let dist = self.width * lines;
+		self.copy_back(0, dist, self.width * self.height - dist);
+		self.position -= dist;
+	}
+
+	fn newline(&mut self) {
+		if self.position > self.width * (self.height - 1) {
+			self.scroll(1);
+		}
+		self.position += self.width - (self.position % self.width);
+		self.clear_eol();
+	}
 }
 
 impl Writer for Console {
-	fn write(&mut self, string : &str) {
-		range(0, string.len(), |i| {
-			let c = string[i] as u16;
-			unsafe {
-				*mut_offset(self.buffer, self.position as int) = self.color | c;
-			}
-			self.position += 1;
-			if self.position > self.width * self.height {
-				// TODO scroll
-				self.position = 0;
-			}
-		});
+	fn putc(&mut self, c : char) {
+		if c == '\n' {
+			self.newline();
+			return;
+		}
+		let value = (c as u8) as u16 | self.color;
+		self.putchar(self.position, value);
+		self.position += 1;
+		if self.position == self.width * self.height {
+			self.scroll(1);
+		}
 	}
 }
 
@@ -87,9 +167,14 @@ impl Writer for Console {
 pub unsafe fn start64() {
 	let mut con = Console::new((kernel_base + 0xb8000) as *mut u16, 80, 25);
 	con.clear();
-	con.write("Hello World! ");
-	writeUInt(&mut con, (*MultiBootInfo()).mem_upper as uint);
-	loop {}
+	con.write("Hello World!\n");
+	con.writeUInt((*MultiBootInfo()).mem_upper as uint);
+	let mut i = 0;
+	loop {
+		con.writeUInt(i);
+		con.putc('\n');
+		i += 1;
+	}
 }
 
 #[no_mangle]
